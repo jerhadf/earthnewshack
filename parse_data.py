b68c25ab00158af9d99ec1863dce7c3e119c259c
gaasher <gabrielasher2002@gmail.com>
2023-04-15 16:07:31 -0400

    changed to chat-gpt engine

--- parse_data.py
+++ parse_data.py
@@ -3,12 +3,11 @@ import numpy as np
 import openai
 import geopy
 from geopy.geocoders import Nominatim
-import extract_geolocation_data
-import utils
+from extract_geolocation_data import get_location_info
 import json
 
 
-openai.api_key = "YOUR_API_KEY" # Replace this with your OpenAI API key
+openai.api_key = "" # Replace this with your OpenAI API key
 
 
 
@@ -16,13 +15,13 @@ def generate_summary(json_article_content):
     input_text = json_article_content['body']
 
     prompt = (f"Please summarize the following text:\n{input_text}\n\nSummary:")
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=2048
+    completion = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = [
+            {'role': 'user', 'content': prompt}
+        ],
     )
-    summary = response.choices[0].text.strip()
+    summary = completion['choices'][0]['message']['content']
 
     data = {"header": "summary", "body": summary}
     json_string = json.dumps(data)
@@ -31,36 +30,38 @@ def generate_summary(json_article_content):
 def extract_loc(json_article_content):
     input_text = json_article_content['body']
     prompt = (f"Please extract the relevant locations in regards to climate change:\n{input_text}\n\. Return these locations in a comma separated list:")
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=2048
-    )
-    summary = response.choices[0].text.split(',')
+    completion = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo',
+            messages = [
+                {'role': 'user', 'content': prompt}
+            ],
+        )
+    locations = completion['choices'][0]['message']['content']
+    locations = locations.split(',')
 
     df = pd.DataFrame(columns=['location', 'address', 'latitude', 'longitude'])
 
-    for i in range(len(summary)):
-        summary[i] = summary[i].strip()
-        location, address, latitude, longitude, raw = extract_geolocation_data.get_location_info(summary[i])
+    for i in range(len(locations)):
+        location, address, latitude, longitude, raw = get_location_info(locations[i])
         df = pd.concat([df, pd.DataFrame({'location': [location], 'address': [address], 'latitude': [latitude], 'longitude': [longitude]})], ignore_index=True)
 
     return df.to_json(orient='records')
 
 
-def extract_explanation(summary, level):
+def extract_explanation(summary, level='beginner'):
     input_text = summary['body']
     prompt = (f"Please explain what is happening in the following text as if you were teaching a {level}. Be sure to include explanations at a {level} level for any relevant key topics.  The text is:\n{input_text}\n\. Return these explanations in a comma separated list:")
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=2048
-    )
-    summary = response.choices[0].text.split(',')
+    completion = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo',
+            messages = [
+                {'role': 'user', 'content': prompt}
+            ],
+        )
+    
+    explanation = completion['choices'][0]['message']['content']
 
-    data = {"header": "explanation", "body": summary, "level": level}
+
+    data = {"header": "explanation", "body": explanation, "level": level}
 
     return data
 

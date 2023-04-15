1d47dacc5d9b4c3beab8623432eaff9bfcde2228
gaasher <gabrielasher2002@gmail.com>
2023-04-15 16:54:09 -0400

    moved extract geolocation to parse_data.py. Also, made it GPT compatible

--- parse_data.py
+++ parse_data.py
@@ -3,9 +3,8 @@ import numpy as np
 import openai
 import geopy
 from geopy.geocoders import Nominatim
-from extract_geolocation_data import get_location_info
 import json
-
+import ast
 
 openai.api_key = "" # Replace this with your OpenAI API key
 
@@ -29,7 +28,7 @@ def generate_summary(json_article_content):
 
 def extract_loc(json_article_content):
     input_text = json_article_content['body']
-    prompt = (f"Please extract the relevant locations in regards to climate change:\n{input_text}\n\. Return these locations in a comma separated list:")
+    prompt = (f"Please extract the relevant locations in regards to climate change:\n{input_text}\n\. Return these locations in a python list format.")
     completion = openai.ChatCompletion.create(
             model = 'gpt-3.5-turbo',
             messages = [
@@ -37,15 +36,14 @@ def extract_loc(json_article_content):
             ],
         )
     locations = completion['choices'][0]['message']['content']
-    locations = locations.split(',')
-
-    df = pd.DataFrame(columns=['location', 'address', 'latitude', 'longitude'])
+    locations = ast.literal_eval(locations)
+    df = pd.DataFrame(columns=['address', 'latitude', 'longitude'])
 
     for i in range(len(locations)):
-        location, address, latitude, longitude, raw = get_location_info(locations[i])
-        df = pd.concat([df, pd.DataFrame({'location': [location], 'address': [address], 'latitude': [latitude], 'longitude': [longitude]})], ignore_index=True)
+        address, latitude, longitude = get_coordinates(locations[i])
+        df = pd.concat([df, pd.DataFrame({'address': [address], 'latitude': [latitude], 'longitude': [longitude]})], ignore_index=True)
 
-    return df.to_json(orient='records')
+    return df, df.to_json(orient='records'), completion
 
 
 def extract_explanation(summary, level='beginner'):
@@ -65,6 +63,12 @@ def extract_explanation(summary, level='beginner'):
 
     return data
 
+
+def get_coordinates(location):
+    geolocator = Nominatim(user_agent="location_script")
+    location_data = geolocator.geocode(location)
+    
+    return location_data.address, location_data.latitude, location_data.longitude
 
 
 

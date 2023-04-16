e4a3544d90957f5f3056f700989d5e25293abbba
jerhadf <jhadfield129@gmail.com>
2023-04-15 21:42:11 -0400

    Added movable map and centered on the Amazon

--- earthnews/src/App.jsx
+++ earthnews/src/App.jsx
@@ -1,31 +1,18 @@
 import { useState } from "react";
 import { useMutation, useQuery } from "../convex/_generated/react";
 import React from "react";
-import Button from "@mui/material/Button";
+import MovableMap from "./MovableMap.jsx";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// marker locations for the movable map demo
+const markers = [
+  { id: 1, lat: 51.505, lng: -0.09, title: "Marker 1", description: "This is marker 1" },
+  { id: 2, lat: 51.51, lng: -0.1, title: "Marker 2", description: "This is marker 2" },
+];
+
 
 export default function App() {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
-
-  // functions from convex - for handling messages
+  // functions from convex - for handling messages - deleted 
   const messages = useQuery("listMessages") || [];
-
-  const [newMessageText, setNewMessageText] = useState("");
-  const sendMessage = useMutation("sendMessage");
-
-  const [name] = useState(() => "User " + Math.floor(Math.random() * 10000));
-  async function handleSendMessage(event) {
-    event.preventDefault();
-    setNewMessageText("");
-    await sendMessage({ body: newMessageText, author: name });
-  }
 
   // put functions for handling articles data and maps here
 
@@ -33,42 +20,31 @@ export default function App() {
   return (
     <main>
       <div className="earth-news">
-
         {/* Header with EarthNews title and background image */}
-        <header
+        {/* <header
           className="header"
-          style={{ backgroundImage: `url(/path/to/your/image.jpg)` }}
+          style={{ backgroundImage: "earthnews/img/americasspace.png" }}
         >
           <h1 className="title">EarthNews</h1>
-        </header>
+        </header> */}
 
         {/* Section for content - map and search bar */}
         <main className="main-content">
-
           {/* Search bar */}
-          <div className="search-container">
+          {/* <div className="search-container">
             <input
               className="search-bar"
               type="text"
               placeholder="Search news..."
             />
             <button className="search-button">Search</button>
-          </div>
+          </div> */}
           {/* <input className="search-bar" type="text" placeholder="Search" /> */}
           {/* Button from Material UI */}
 
           {/* <YourMapComponent /> */}
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43934475.96055831!2d63.33668226800676!3d47.77082869327741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x453c569a896724fb%3A0x1409fdf86611f613!2sRussia!5e0!3m2!1sen!2sus!4v1681587236497!5m2!1sen!2sus"
-            width="1000"
-            height="950"
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-
+          <MovableMap markers={markers} />
         </main>
-
       </div>
     </main>
   );
---
+++ earthnews/src/MovableMap.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix the Leaflet icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+const MovableMap = ({ markers }) => {
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  return (
+    // The MapContainer starts zoomed into the Amazon Rainforest 
+    <MapContainer className="MovableMap" center={[-3.4, -60.6]} zoom={3} style={{ width: "100%", height: "100%" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markers.map((marker) => (
+        <Marker
+          key={marker.id}
+          position={[marker.lat, marker.lng]}
+          eventHandlers={{
+            click: () => {
+              setSelectedMarker(marker);
+            },
+          }}
+        />
+      ))}
+      {selectedMarker && (
+        <Popup
+          position={[selectedMarker.lat, selectedMarker.lng]}
+          onClose={() => setSelectedMarker(null)}
+        >
+          <div>
+            <h2>{selectedMarker.title}</h2>
+            <p>{selectedMarker.description}</p>
+          </div>
+        </Popup>
+      )}
+    </MapContainer>
+  );
+};
+
+export default MovableMap;

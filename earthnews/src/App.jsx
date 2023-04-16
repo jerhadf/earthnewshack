d761d567be0cb786a18b9ee6d3be2415cc725cbc
jerhadf <jhadfield129@gmail.com>
2023-04-15 21:11:59 -0400

    Reorganized css to make it workable

--- earthnews/src/App.jsx
+++ earthnews/src/App.jsx
@@ -26,86 +26,50 @@ export default function App() {
     setNewMessageText("");
     await sendMessage({ body: newMessageText, author: name });
   }
-  
+
   // put functions for handling articles data and maps here
 
   // FRONTEND STARTS HERE - REACT COMPONENTS + HTML
   return (
     <main>
-      <div className="container">
-        <div className="earth-news">
-          {/* Header with EarthNews title and background image */}
-          <header
-            className="header"
-            style={{ backgroundImage: `url(/path/to/your/image.jpg)` }}
-          >
-            <h1 className="title">EarthNews</h1>
-          </header>
-          {/* Section for map and search bar */}
-          <main className="main-content">
-            <div className="search-container">
-              <input
-                className="search-bar"
-                type="text"
-                placeholder="Search news..."
-              />
-              <button className="search-button">Search</button>
-            </div>
-            {/* <YourMapComponent /> */}
-            {/* Search bar */}
-            {/* <input className="search-bar" type="text" placeholder="Search" /> */}
-            {/* Button from Material UI */}
-          </main>
-        </div>
+      <div className="earth-news">
 
-        {/* Deprecated - Messages Stuff - FROM CONVEX DEMO */}
-        <p className="badge">
-          <span>{name}</span>
-        </p>
-        <ul>
-          {messages.map((message) => (
-            <li key={message._id.toString()}>
-              <span>{message.author}:</span>
-              <span>{message.body}</span>
-              <span>
-                {new Date(message._creationTime).toLocaleTimeString()}
-              </span>
-            </li>
-          ))}
-        </ul>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43934475.96055831!2d63.33668226800676!3d47.77082869327741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x453c569a896724fb%3A0x1409fdf86611f613!2sRussia!5e0!3m2!1sen!2sus!4v1681587236497!5m2!1sen!2sus"
-          width="1000"
-          height="950"
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        {/* Header with EarthNews title and background image */}
+        <header
+          className="header"
+          style={{ backgroundImage: `url(/path/to/your/image.jpg)` }}
+        >
+          <h1 className="title">EarthNews</h1>
+        </header>
+
+        {/* Section for content - map and search bar */}
+        <main className="main-content">
+
+          {/* Search bar */}
+          <div className="search-container">
+            <input
+              className="search-bar"
+              type="text"
+              placeholder="Search news..."
+            />
+            <button className="search-button">Search</button>
+          </div>
+          {/* <input className="search-bar" type="text" placeholder="Search" /> */}
+          {/* Button from Material UI */}
+
+          {/* <YourMapComponent /> */}
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43934475.96055831!2d63.33668226800676!3d47.77082869327741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x453c569a896724fb%3A0x1409fdf86611f613!2sRussia!5e0!3m2!1sen!2sus!4v1681587236497!5m2!1sen!2sus"
+            width="1000"
+            height="950"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+
+        </main>
+
       </div>
-      <form onSubmit={handleSendMessage}>
-        <input
-          value={newMessageText}
-          onChange={(event) => setNewMessageText(event.target.value)}
-          placeholder="Write a message…"
-        />
-        <input type="submit" value="Send" disabled={!newMessageText} />
-      </form>
-      <form onSubmit={handleSendMessage}>
-        <input
-          value={newMessageText}
-          onChange={(event) => setNewMessageText(event.target.value)}
-          placeholder="Write a message…"
-        />
-        <input type="submit" value="Send" disabled={!newMessageText} />
-      </form>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43934475.96055831!2d63.33668226800676!3d47.77082869327741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x453c569a896724fb%3A0x1409fdf86611f613!2sRussia!5e0!3m2!1sen!2sus!4v1681587236497!5m2!1sen!2sus"
-        width="1000"
-        height="950"
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
     </main>
   );
 }

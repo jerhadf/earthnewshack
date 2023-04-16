258473b0b729cf846ef98d81dffa4f267b020587
shivangitandon <shivitandon70@gmail.com>
2023-04-15 20:26:35 -0400

    added background

--- earthnews/src/App.jsx
+++ earthnews/src/App.jsx
@@ -2,18 +2,11 @@ import { useState } from "react";
 import { useMutation, useQuery } from "../convex/_generated/react";
 import React from "react";
 import GoogleMapReact from 'google-map-react';
-import Button from '@mui/material/Button';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function App() {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
+
   const messages = useQuery("listMessages") || [];
 
   const [newMessageText, setNewMessageText] = useState("");
@@ -27,25 +20,12 @@ export default function App() {
   }
   return (
     <main>
-      {/* Button from Material UI */}
-      <div>
-        <Button variant="contained">Hello World</Button>
+    <div className="container">
+      <h1 className="title">EarthNews</h1>
+      <div className="search-container">
+        <input className="search-bar" type="text" placeholder="Search news..." />
+        <button className="search-button">Search</button>
       </div>
-      
-      <div>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
-      <h1>EarthNews</h1>
       <p className="badge">
         <span>{name}</span>
       </p>
@@ -58,15 +38,16 @@ export default function App() {
           </li>
         ))}
       </ul>
-      <form onSubmit={handleSendMessage}>
-        <input
-          value={newMessageText}
-          onChange={event => setNewMessageText(event.target.value)}
-          placeholder="Write a message…"
-        />
-        <input type="submit" value="Send" disabled={!newMessageText} />
-      </form>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43934475.96055831!2d63.33668226800676!3d47.77082869327741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x453c569a896724fb%3A0x1409fdf86611f613!2sRussia!5e0!3m2!1sen!2sus!4v1681587236497!5m2!1sen!2sus" width="1000" height="950" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-    </main>
-  );
-}
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43934475.96055831!2d63.33668226800676!3d47.77082869327741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x453c569a896724fb%3A0x1409fdf86611f613!2sRussia!5e0!3m2!1sen!2sus!4v1681587236497!5m2!1sen!2sus" width="1000" height="950" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <form onSubmit={handleSendMessage}>
+         <input
+           value={newMessageText}
+           onChange={event => setNewMessageText(event.target.value)}
+           placeholder="Write a message…"
+         />
+         <input type="submit" value="Send" disabled={!newMessageText} />
+       </form>
+       </main>
+    )};
+
